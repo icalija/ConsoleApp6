ea84d0d8febee8bc6f86bdb0575703fd99bd9ace
Igor Čalija <icalija@live.com>
2018-03-28 02:41:30 +0200

    promena

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -9,15 +9,15 @@ namespace ConsoleApp6
     class Program
     {
 
-        public int Swap(int a, int b)
-        {
-            int temp;
-            temp = a;
-            a = b;
-            b = temp;
-            return temp;
+        //public int Swap(int a, int b)
+        //{
+        //    int temp;
+        //    temp = a;
+        //    a = b;
+        //    b = temp;
+        //    return temp;
             
-        }
+        //}
 
 
 
@@ -42,15 +42,15 @@ namespace ConsoleApp6
 
             //}
 
-            Program n = new Program();
-            int a = 100;
-            int b = 200;
-            Console.WriteLine("Pre zamenje vrednost a je {0}", a);
-            Console.WriteLine("Pre zamene vrednost b je {0}", b);
+            //Program n = new Program();
+            //int a = 100;
+            //int b = 200;
+            //Console.WriteLine("Pre zamenje vrednost a je {0}", a);
+            //Console.WriteLine("Pre zamene vrednost b je {0}", b);
 
-            n.Swap(b, a);
-            Console.WriteLine("Nakon zamene vrednost a je {0}", a);
-            Console.WriteLine("Nakon zamene vrednost b je {0}", b);
+            //n.Swap(b, a);
+            //Console.WriteLine("Nakon zamene vrednost a je {0}", a);
+            //Console.WriteLine("Nakon zamene vrednost b je {0}", b);
 
             //int a, b, temp;
             //Console.WriteLine("Vrednost a je");
@@ -63,6 +63,64 @@ namespace ConsoleApp6
             //b = temp;
 
             //Console.WriteLine("Vrednost a je {0}, a vrednost b je {1}", a, b);
+
+
+
+            ////inicijalizacija niza stringova od 25 elemenata
+            //string[] niz = new string[26];
+            //for (int i = 1; i < niz.Length; i++)
+            //{
+            //    niz[i] = "Elemenat " + i;
+            //}
+
+            //foreach (var elemenat in niz)
+            //{
+            //    Console.WriteLine(" ");
+            //    Console.WriteLine(elemenat);
+            //}
+
+            //int[] niz = new int[5];
+            //string[] niz2 = new string[8];
+            //string[] array = {"ja"};
+            //int[] array2 = { 1, 2, 3 };
+
+
+            ////niz = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("{0}", array2[1]);
+
+
+
+
+
+            //int x;
+            //Console.WriteLine("Upisati broj od 1 do 50");
+            //x = Convert.ToInt32(Console.ReadLine());
+            ////x = int.Parse(Console.ReadLine());
+
+
+            //if (x % 3 == 0 && x % 5 == 0)
+            //{
+            //    Console.WriteLine("Cica Maca");
+            //}
+
+            //else if (x % 3 == 0)
+            //{
+            //    Console.WriteLine("Cica");
+            //}
+
+            //else if (x % 5 == 0)
+            //{
+            //    Console.WriteLine("Maca");
+            //}
+
+            //int[] n = new int[10];
+            //int i, j;
+
+            //for (i = 0; i< 10; i++)
+            //{
+            //    Console.WriteLine("vrednosti n su {0} = {1}", i, n[i]);
+            //}
+
 
             Console.ReadKey();
         }
